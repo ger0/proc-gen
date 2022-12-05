208ce11777f0623468bcfd5685d0de902989a227
ger0 <mrgero155@gmail.com>
2022-12-05 21:21:49 +0100

    Lookup table

--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,24 @@
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
+#include <array>
 #include <memory>
 
 #include "SimplexNoise/SimplexNoise.h"
 #include "lodepng/lodepng.h"
 #include "shaderprogram.hpp"
+#include "marchingcubes.hpp"
 
-using uint = unsigned;
 using byte = unsigned char;
+using uint = unsigned;
+
+#define DEBUG
 
 // unique_ptr alias
 template<typename... T>
 using Uniq_Ptr = std::unique_ptr<T...>;
+
+using std::array;
 
 constexpr const char* 	WINDOW_TITLE = "GLTRY";
 constexpr uint 		WINDOW_W = 800;
@@ -36,6 +42,7 @@ constexpr float Z_FAR = MAP_W;
 
 constexpr uint NOISE_W = MAP_W + 1;
 constexpr uint NOISE_H = MAP_H + 1;
+constexpr float noiseScale = 32.f;
 
 template<typename T>
 struct Position {
@@ -70,11 +77,14 @@ float pitch = 0.f;
 // mouse position
 Position<float> mouseLast;
 
-std::vector<float> noise(NOISE_W * NOISE_H);
+array<float, NOISE_W * NOISE_H> noise;
+
+array<float, NOISE_W * NOISE_H * 32> noise3D;
 
 // vertex + normal data (6 + 6)
 // {pos{x, y, z}, norm{x, y, z}} for every single vertex
-std::vector<glm::vec3> verts((6 + 6) * MAP_W * MAP_H);
+array<glm::vec3, (6 + 6) * MAP_W * MAP_H> verts;
+array<glm::vec3, (6 + 6) * MAP_W * MAP_W * MAP_H> verts3D;
 
 const float cameraSpeed = 0.05f;
 
@@ -227,15 +237,16 @@ int printFail(const char *message) {
 }
 
 inline float& noiseAt(Position<uint> pos) {
-	return noise[pos.x + pos.y * NOISE_W];
+	// bounds checking
+	return noise.at(pos.x + pos.y * NOISE_W);
 }
 
 // generates a normal vector for a single vertex by 
 // checking noise values around the vertex
 void genNormal(uint offset, Position<uint> pos) {
 	// ignore vertices on the edge of the map
-    if (!(pos.x > 0 && pos.y > 0 && pos.x < MAP_W && pos.y < MAP_H)) {
-        verts[offset + 1] = glm::vec3(0.f, -1.f, 0.f);
+    if (!(pos.x > 0 && pos.y > 0 && pos.x < (MAP_W) && pos.y < (MAP_H))) {
+        verts.at(offset + 1) = glm::vec3(0.f, -1.f, 0.f);
         return;
     }
 
@@ -248,13 +259,13 @@ void genNormal(uint offset, Position<uint> pos) {
     glm::vec3 norm;
 	norm = glm::cross(dz, dx);
 	norm = glm::normalize(norm);
-	verts[offset + 1] = norm;
+	verts.at(offset + 1) = norm;
 }
 
 // generates mesh with normals out of a noise into verts vector
 void genMesh() {
 	constexpr uint step = 12; // 6 vertices + 6 normals
-	for (uint i = 0; i < (MAP_H * MAP_W) * step; i+= step) {
+	for (uint i = 0; i < (MAP_H * MAP_W - step) * step; i+= step) {
 		uint frac = i / step;
 		Position<uint> pos = {frac % MAP_W, frac / MAP_W};
 		/* vertice offsets to create 2 polygons for one quad
@@ -277,50 +288,91 @@ void genMesh() {
 			// here the 3D Z coord is mapped to the Y coord on the 2D noise plane
 			// the Y coord is the noise value at (X, Z)
 			vert.z = pos.y + offset.y;
-			verts[i + idx] = vert;
+			verts.at(i + idx) = vert;
 			idx += 2;
 			genNormal(i + idx, pos);
 		}
 	}
 }
 
+void genMesh3D() {
+	constexpr uint step = 12; // 6 vertices + 6 normals
+
+}
+
+void saveNoisePNG(const char* fname, array<float, NOISE_W * NOISE_H>& noise) {
+	array<byte, NOISE_W * NOISE_H> picture;
+	float max = -99999.f;
+	for (uint i = 0; i < noise.size(); i++) {
+		auto val = noise.at(i) * 128.f / noiseScale;
+		if (val > max) max = val;
+		picture.at(i) = noise.at(i) * 128.f / noiseScale;
+	}
+	printf("MAXXXX: %f\n", max);
+    lodepng_encode_file(fname, picture.data(), 
+    		NOISE_W, NOISE_H, LCT_GREY, 8);
+}
+
 void genWavesMap() {
 	auto val = 0.f;
-    std::vector<byte> picture(NOISE_W * NOISE_H);
 	for (uint y = 0; y <= MAP_H; y++) {
 		val += 0.5f;
 		if (y % 32 == 0) val = 0.f;
     	for (uint x = 0; x <= MAP_W; x++) {
 			noiseAt({x, y}) = val;
-            picture[x + y * NOISE_W] = 128 * val;
 		}
 	}
-	// DEBUG SAVE
-    lodepng_encode_file("wave.png", picture.data(), 
-    		NOISE_W, NOISE_H, LCT_GREY, 8);
+#ifdef DEBUG
+	saveNoisePNG("waves.png", noise);
+#endif
 }
 
 void genSimplexMap() {
 	SimplexNoise noiseGen(3.f / MAP_W, 1.f, 2.f, 0.5f);
-    std::vector<byte> picture(NOISE_W * NOISE_H);
 	// noise generation
 	for (uint y = 0; y <= MAP_H; y++) {
     	for (uint x = 0; x <= MAP_W; x++) {
-    		auto val = noiseGen.fractal(4, x, y) * 0.5f + 0.5f;
-			noiseAt({x, y}) = 32.f * val;
-
-            picture[x + y * NOISE_W] = 128 * val;
+    		auto val = noiseGen.fractal(4, x, y) + 1.f;
+			noiseAt({x, y}) = noiseScale * val;
 		}
 	}
-    // DEBUG SAVE
-    lodepng_encode_file("noise.png", picture.data(), 
-    		NOISE_W, NOISE_H, LCT_GREY, 8);
+#ifdef DEBUG
+    saveNoisePNG("noise.png", noise);
+#endif
+}
+
+void fill3DNoise(array<float, NOISE_W * NOISE_H * 32>& noise) {
+	SimplexNoise noiseGen(3.f / MAP_W, 1.f, 2.f, 0.5f);
+	// noise generation
+	for (uint y = 0; y < 32; y++) {
+		array<byte, NOISE_W * NOISE_H> picture;
+		for (uint z = 0; z <= MAP_H; z++) {
+			for (uint x = 0; x <= MAP_W; x++) {
+				auto val = noiseGen.fractal(4, x, y, z);
+				noise.at(y * (NOISE_W * NOISE_H) + 
+					(x + z * NOISE_W)) = noiseScale * val;
+			}
+		}
+#ifdef DEBUG
+		// outputnoise directory must exist
+		constexpr uint size = NOISE_W * NOISE_H;
+		array<float, size> noiseSlice;
+		std::copy(noise.begin() + size * y,
+				noise.begin() + size * (y + 1),
+				noiseSlice.begin());
+		char str[21 + 48 / 8];
+		sprintf(str, "outputnoise/noise-%u.png", y);
+		saveNoisePNG(str, noiseSlice);
+#endif
+	}
 }
 
 int main() {
 	// generate a heightmap and a mesh out of it
 	genSimplexMap();
 	genMesh();
+
+	fill3DNoise(noise3D);
 
 	glfwSetErrorCallback(errCallback);
 	if (!glfwInit()) {
@@ -347,7 +399,7 @@ int main() {
 	if (glewInit() != GLEW_OK) return printFail("Cannot initialise GLEW.");
 
 	auto shaderDestroyer = [&](ShaderProgram* sp) {
-		std::array<GLuint, 2> buffers({vao, vbo});
+		array<GLuint, 2> buffers({vao, vbo});
 		glDeleteBuffers(buffers.size(), buffers.data());
 		delete sp;
 	};
